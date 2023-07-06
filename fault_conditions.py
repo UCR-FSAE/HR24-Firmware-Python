b7e79d9c2752792d446f9db097433034277722f4
hernantech <58359753+hernantech@users.noreply.github.com>
2023-07-05 19:22:08 -0700

    adding fault check skeleton

--- fault_conditions.py
+++ fault_conditions.py
@@ -8,6 +8,13 @@ class fault_conditions():
         print("fault code check initialized")
     def check(self, code):
         #check code against table
-        #likely needs optimization, maybe a bloom filter, maybe a hash table
+        #likely needs optimization, maybe a preliminary bloom filter, maybe a hash table
+            #bloom filter would run quick search for code NOT being fault code
+            #false return would trigger long search and if found, return interrupt
+
         #leaving dictionary implementation as default
+        if code in config.FaultCodes.keys():
+            return True
+        else:
+            return False
 
