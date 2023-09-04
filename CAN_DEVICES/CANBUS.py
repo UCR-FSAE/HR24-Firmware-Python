fb0c67917671e802571f3746820e37c08133224a
Mario Cuaya <mcuay001@ucr.edu>
2023-09-04 12:12:49 -0700

    adding canbus_config.ini file

--- CAN_DEVICES/CANBUS.py
+++ CAN_DEVICES/CANBUS.py
@@ -6,17 +6,27 @@ https://python-can.readthedocs.io/en/master/bus.html#thread-safe-bus
 
 '''
 import can
-import canbusconfig
 #import canbusconfig
+#import canbusconfig
+
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('canbus_config.ini')
+
 #class CANBUS(canbusconfig) not working when having in
 class CANBUS():
     def __init__(self):
         try:
             print("setup virtal CANBUS")
-            self.first_bus = can.ThreadSafeBus(interface=canbusconfig.interface['socketcan'], channel=canbusconfig.ports['virtual'])
+            #self.first_bus = can.ThreadSafeBus(interface=canbusconfig.interface['socketcan'], channel=canbusconfig.ports['virtual'])
             #self.first_bus.recv(timeout=3) #if it spends more than
-
+            self.first_bus = can.ThreadSafeBus(interface=config['Inferfaces']['SocketCan'], channel=config['Ports']['virtual'])
         except:
             e = "error setting up the threadsafebus, please ensure that your interface"
+            print(e)
             return
-    
+
+#print(list(config['Inferfaces']))
+
+#can_bus_test = CANBUS()
--- connections.py
+++ connections.py
@@ -1,17 +1,16 @@
 import can
-import sys
-sys.path.append('/home/mario/Desktop/HR24E_python/CAN_DEVICES')
-
 #import CANBUS
 from CAN_DEVICES.CANBUS import CANBUS
 
 class connections:
-    def checkCANBUS(self,CANBUS):
+    @classmethod
+    def checkCANBUS(cls):
         canbusCheck = CANBUS()
         canbusCheck.first_bus.send(can.Message(arbitration_id=123, is_extended_id=True, data=[0x11, 0x22, 0x33]))
-        canbusCheck.first_bus.recv(timeout=3)
-        canbusCheck.first_bus.shutdown()
+        #canbusCheck.first_bus.recv(timeout=3)
+        #canbusCheck.first_bus.shutdown()
         print("connection checked")
         
 
-connections.checkCANBUS(connections, CANBUS)
+connections.checkCANBUS()
+
