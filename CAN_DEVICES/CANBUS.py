5a9d073c89c8c34c7a5c68aa42b72e40700b786c
sidd <sidd@sidds-MacBook-Pro.local>
2023-11-18 14:26:01 -0800

    modified connections

--- CAN_DEVICES/CANBUS.py
+++ CAN_DEVICES/CANBUS.py
@@ -15,7 +15,8 @@ config.read('canbus_config.ini')
 class CANBUS():
     def __init__(self, listener_list:list):
         try:
-            self.first_bus = can.ThreadSafeBus(interface=config['Inferfaces']['SocketCan'], channel=config['Ports']['virtual'], receive_own_messages=True)
+            # self.first_bus = can.ThreadSafeBus(interface=config['Inferfaces']['SocketCan'], channel=config['Ports']['virtual'], receive_own_messages=True)
+            self.first_bus = can.ThreadSafeBus('test', bustype='virtual', receive_own_messages=True)
             self.can_notifer = can.Notifier(self.first_bus, listener_list)
             print("virtual bus set")
         except:
@@ -34,8 +35,9 @@ class CANBUS():
     #function to shutdown canbus system
     def shutdown(self):
         print("shutting down")
-        self.temp_listner.stop()
-        self.mail_box.stop()
+        # self.temp_listner.stop()
+        # self.mail_box.stop()
+        self.can_notifer.stop()
         self.first_bus.shutdown()
 
 
--- connections.py
+++ connections.py
@@ -2,11 +2,18 @@ import can
 from can.message import Message
 from CAN_DEVICES.CANBUS import CANBUS
 import time
+import random
+import datetime
+
+test_bus = can.ThreadSafeBus('test', bustype='virtual')
+
+id_list = random.sample(range(1000), 10)
 
 #simply listener class to test connection of canbus
 class test_listener(can.BufferedReader):
     def on_message_received(self, msg: Message):
         print({msg.arbitration_id})
+        handshake_dict[msg.arbitration_id] = msg.data
 
 class connections:
     @classmethod
@@ -14,20 +21,60 @@ class connections:
         temp_listener = test_listener()
         temp_listener_list = [temp_listener]
         canbus_system = CANBUS(temp_listener_list)
-        canbus_system.send_message(can.Message(arbitration_id=110, is_extended_id=False, data=[11,22,33,44,55]))
-        canbus_system.send_message(can.Message(arbitration_id=111, is_extended_id=False, data=[11,22,33,44,55]))
-        canbus_system.send_message(can.Message(arbitration_id=112, is_extended_id=False, data=[11,22,33,44,55]))
+        # canbus_system.send_message(can.Message(arbitration_id=110, is_extended_id=False, data=[11,22,33,44,55]))
+        # canbus_system.send_message(can.Message(arbitration_id=111, is_extended_id=False, data=[11,22,33,44,55]))
+        # canbus_system.send_message(can.Message(arbitration_id=112, is_extended_id=False, data=[11,22,33,44,55]))
+        
+        msg = can.Message(arbitration_id=110, is_extended_id=False, data=[11,22,33,44,55])
+        canbus_system.send_message(msg)
 
         #buffer to allow time for messages to be received and printed
         time.sleep(1)
-        canbus_system.shutdown()
-        print("connection checked")
-
         #possbily use on recive to check if message is a fault
         #code and if it is return false for tick functionality
+
+        # CHECK IF CONNECTED
+        print("checking connection")
+        # new_msg = canbus_system.recv(timeout=5)
+        # print(new_msg.arbitration_id)
+        print("connection checked")
+
+        # COLLECT DATA
+        x = time.time()
+        timeout = x + 20
+        i = 0
+        while True:
+            if time.time() > timeout:
+                break
+            if i < 10:
+                canbus_system.send_message(can.Message(arbitration_id=id_list[i], 
+                                                        is_extended_id=False, 
+                                                        data=[11,22,33,44,55]))
+                i+=1
+                # received_msg = canbus_system.recv(timeout=5)  
+
+
+        canbus_system.shutdown()
+
+        
         
 
 
+handshake_dict = {}
         
 connections.checkCANBUS()
+test_bus.shutdown()
 
+# print(handshake_dict)
+
+# CHECK IF ID IS IN DICT
+id_1 = id_list[0]
+id_2 = id_list[1]
+if id_1 in handshake_dict:
+    print("id " + str(id_1) + " found in dict")
+else:
+    print('id: ' + str(id_1) + ' not found')
+if id_2 in handshake_dict:
+    print("id " + str(id_2) + " found in dict")
+else:
+    print('id: ' + str(id_2) + ' not found')
